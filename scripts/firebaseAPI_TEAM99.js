7532dfd264eb4e71e1a248382bdb08b86a509aeb
ramsay-e <90486408+ramsay-e@users.noreply.github.com>
2021-11-16 04:23:56 -0800

    Add files via upload

---
+++ scripts/firebaseAPI_TEAM99.js
@@ -0,0 +1,15 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyDwY2Ym3vkMtZoPR21Vm8f7tKZMh0oaof8",
+  authDomain: "demo08-ramsay.firebaseapp.com",
+  projectId: "demo08-ramsay",
+  storageBucket: "demo08-ramsay.appspot.com",
+  messagingSenderId: "84156438117",
+  appId: "1:84156438117:web:dce2ee2ee3556d3563588c"
+};
+
+
+
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+
